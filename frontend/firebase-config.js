795e169093608a8c4f3298c9bddc7b66393be061
Lana Mustafić <lana.mustafic@edu.fit.ba>
2026-08-19 02:12:19 +0200

    fix: update firebase-config to target sistemzalivanje database

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
+/**
+ * Must match ESP32 FIREBASE_HOST in SmartSprinklerIoT/config.h:
+ * sistemzalivanje-default-rtdb.europe-west1.firebasedatabase.app
+ */
 const firebaseConfig = {
   apiKey: "AIzaSyC0039OXDbhmjGZgxrKY2uKKqarn2KSOvE",
-  authDomain: "smartsprinkleriot-d2e5c.firebaseapp.com",
-  databaseURL: "https://smartsprinkleriot-d2e5c-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "smartsprinkleriot-d2e5c",
-  storageBucket: "smartsprinkleriot-d2e5c.firebasestorage.app",
+  authDomain: "sistemzalivanje.firebaseapp.com",
+  databaseURL: "https://sistemzalivanje-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "sistemzalivanje",
+  storageBucket: "sistemzalivanje.firebasestorage.app",
   messagingSenderId: "983649039756",
-  appId: "1:983649039756:web:7cbd124b6b8efa744652a9"
+  appId: "1:983649039756:web:7cbd124b6b8efa744652a9",
 };
 
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const database = getDatabase(app, firebaseConfig.databaseURL);
 
 export { database };
